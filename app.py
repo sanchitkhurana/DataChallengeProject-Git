2b2100ed64b86f6743ebc83a4bb020d156653a3a
Prathamesh Khandade <tosheetk@gmail.com>
2022-07-22 22:07:47 -0400

    fourth commit

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
 from flask import Flask ,render_template, request, redirect, url_for
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/Searchbooks')
+def searchbooks():
+    return render_template('spselection.html')
+
+@app.route("/Topfiftybooks")
+def topfiftybooks():
+    return render_template('topbooks.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
