888eb8b0fca606bff9dd710f63ac4bb14cb1ec92
Prathamesh Khandade <tosheetk@gmail.com>
2022-07-28 12:49:24 -0400

    sixth commit

--- app.py
+++ app.py
@@ -22,17 +22,22 @@ def index():
 def recommend():
     if request.method == 'POST':
         user_input = request.values['user_input']
-        index = np.where(pt.index == user_input)[0][0]
-        similar_items = sorted(list(enumerate(similarity_scores[index])), key=lambda x: x[1], reverse=True)[1:5]
         data = []
-        for i in similar_items:
-            item = []
-            temp_df = books[books['Book-Title'] == pt.index[i[0]]]
-            item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
-            item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values))
-            item.extend(list(temp_df.drop_duplicates('Book-Title')['Image-URL-M'].values))
-            data.append(item)
-        return render_template('recommend.html',data=data)
+        if(len(np.where(pt.index == user_input)[0])):
+            index = np.where(pt.index == user_input)[0][0]
+            similar_items = sorted(list(enumerate(similarity_scores[index])), key=lambda x: x[1], reverse=True)[1:5]
+            
+            for i in similar_items:
+                item = []
+                temp_df = books[books['Book-Title'] == pt.index[i[0]]]
+                item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
+                item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values))
+                item.extend(list(temp_df.drop_duplicates('Book-Title')['Image-URL-M'].values))
+                data.append(item)
+            f = 1
+        else:
+            f = 0
+        return render_template('recommend.html',data=data,f =f)
     else:
         return render_template('recommend.html')
 
