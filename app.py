d144e44cdaa55ad7a4d1ee8ce5c9d31b554679b7
Prathamesh Khandade <tosheetk@gmail.com>
2022-07-27 22:41:14 -0400

    fifth commit

--- app.py
+++ app.py
@@ -1,9 +1,41 @@
+from distutils.log import debug
 from flask import Flask ,render_template, request, redirect, url_for
+import numpy as np
+import pickle
+
+popular_df = pickle.load(open('popular.pkl','rb'))
+pt = pickle.load(open('pt.pkl','rb'))
+books = pickle.load(open('books.pkl','rb'))
+similarity_scores = pickle.load(open('similarity_scores.pkl','rb'))
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',book_name = list(popular_df['Book-Title'].values),
+                            author = list(popular_df['Book-Author'].values),
+                            image = list(popular_df['Image-URL-M'].values),
+                            votes = list(popular_df['number_of_ratings'].values),
+                            rating = list(popular_df['average_ratings'].values))
+
+@app.route('/Recommend',methods=["GET","POST"])
+def recommend():
+    if request.method == 'POST':
+        user_input = request.values['user_input']
+        index = np.where(pt.index == user_input)[0][0]
+        similar_items = sorted(list(enumerate(similarity_scores[index])), key=lambda x: x[1], reverse=True)[1:5]
+        data = []
+        for i in similar_items:
+            item = []
+            temp_df = books[books['Book-Title'] == pt.index[i[0]]]
+            item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
+            item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values))
+            item.extend(list(temp_df.drop_duplicates('Book-Title')['Image-URL-M'].values))
+            data.append(item)
+        return render_template('recommend.html',data=data)
+    else:
+        return render_template('recommend.html')
+
 
 @app.route('/Searchbooks')
 def searchbooks():
@@ -11,7 +43,11 @@ def searchbooks():
 
 @app.route("/Topfiftybooks")
 def topfiftybooks():
-    return render_template('topbooks.html')
+    return render_template('topbooks.html',book_name = list(popular_df['Book-Title'].values),
+                            author = list(popular_df['Book-Author'].values),
+                            image = list(popular_df['Image-URL-M'].values),
+                            votes = list(popular_df['number_of_ratings'].values),
+                            rating = list(popular_df['average_ratings'].values))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug = True)
