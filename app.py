a34eb5fb45ff4df0a8185449fe35d227157f40d7
Prathamesh Khandade <tosheetk@gmail.com>
2022-07-30 16:30:05 -0400

    final commit

--- app.py
+++ app.py
@@ -1,22 +1,32 @@
 from distutils.log import debug
-from flask import Flask ,render_template, request, redirect, url_for
+from flask import Flask ,render_template, request, redirect, url_for, jsonify
 import numpy as np
 import pickle
+from flask_cors import CORS
+from chat import get_response
 
 popular_df = pickle.load(open('popular.pkl','rb'))
 pt = pickle.load(open('pt.pkl','rb'))
 books = pickle.load(open('books.pkl','rb'))
 similarity_scores = pickle.load(open('similarity_scores.pkl','rb'))
+pop_sort = pickle.load(open('pop_sort.pkl','rb'))
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html',book_name = list(popular_df['Book-Title'].values),
-                            author = list(popular_df['Book-Author'].values),
-                            image = list(popular_df['Image-URL-M'].values),
-                            votes = list(popular_df['number_of_ratings'].values),
-                            rating = list(popular_df['average_ratings'].values))
+    return render_template('index.html',image = list(popular_df['Image-URL-M'].values),
+                            sp_image = list(pop_sort['Image-URL-M'].values))
+
+
+@app.post("/predict")
+def predict():
+    text=request.get_json().get("message")
+    #TODO:check if text is valid
+    response=get_response(text)
+    message={"answer":response}
+    return jsonify(message)
 
 @app.route('/Recommend',methods=["GET","POST"])
 def recommend():
@@ -44,7 +54,11 @@ def recommend():
 
 @app.route('/Searchbooks')
 def searchbooks():
-    return render_template('spselection.html')
+    return render_template('spselection.html',book_name = list(pop_sort['Book-Title'].values),
+                            author = list(pop_sort['Book-Author'].values),
+                            image = list(pop_sort['Image-URL-M'].values),
+                            votes = list(pop_sort['number_of_ratings'].values),
+                            rating = list(pop_sort['average_ratings'].values))
 
 @app.route("/Topfiftybooks")
 def topfiftybooks():
